8e3ecf2d9d5c3eb1386b6835c87296da8f4bc776
Gonzalo Martone <gmartone@itba.edu.ar>
2022-09-15 12:42:21 -0300

    FIX: set default if env SO_IMAGE is not set

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+if [ -z $SO_IMAGE ]; then
+    SO_IMAGE='agodio/itba-so:1.0'
+fi
+
 name=TP2SO
 # abro docker y ejecuto los make necesarios
 docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name $name $SO_IMAGE
 docker exec -it $name make clean        --directory=/root/Toolchain
 docker exec -it $name make all          --directory=/root/Toolchain
 docker exec -it $name make clean        --directory=/root
-if [ "$1" == "gdb" ]
-then
+if [ "$1" == "gdb" ]; then
     docker exec -it $name make gdb      --directory=/root
 else
     docker exec -it $name make all      --directory=/root
@@ -17,8 +21,7 @@ docker stop $name
 docker rm $name
 
 # --rtc base=localtime para q este correctamente seteado el RTC
-if [ "$1" == "gdb" ]
-then
+if [ "$1" == "gdb" ]; then
     # modo debug (hay que hacer attach mediante gdb en otra imagen)
     qemu-system-x86_64 -S -s --rtc base=localtime -hda Image/x64BareBonesImage.qcow2 -m 512 -d int -monitor stdio
 else
