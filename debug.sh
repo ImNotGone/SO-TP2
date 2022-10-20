0c674f005829795cc196e49dc81781c2496e9ea8
Gonzalo Martone <gmartone@itba.edu.ar>
2022-10-19 21:14:35 -0300

    DEV: debug.sh supports SO_IMAGE env variable

--- debug.sh
+++ debug.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 docker stop gdb
 docker rm gdb
-docker run -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name gdb agodio/itba-so:1.0
+
+if [ -z $SO_IMAGE ]; then
+    SO_IMAGE='agodio/itba-so:1.0'
+fi
+
+docker run -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name gdb $SO_IMAGE
 docker stop gdb
 docker rm gdb
