52f79cb81ec555597751332a6cfaaf3e870d4e68
Gonzalo Martone <gmartone@itba.edu.ar>
2022-10-24 17:06:39 -0300

    CLEAN: Move process.h

--- Kernel/include/libs/processManager.h
+++ Kernel/include/libs/processManager.h
@@ -2,10 +2,10 @@
 #define PROCESSMANAGER_H
 
 #include <stdint.h>
-#include <process.h>
+#include <libs/process.h>
 
 // C functions
-uint64_t newProcess( uint64_t rip, int argc, char * argv[]);
+uint64_t newProcess(uint64_t rip, int argc, char * argv[]);
 
 uint64_t schedule(uint64_t rsp);
 
