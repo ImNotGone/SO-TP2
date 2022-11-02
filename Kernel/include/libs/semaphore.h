8043d89a0f5da200f09ba201d433134b3c6e091b
Cuinardium <sballerini@itba.edu.ar>
2022-11-01 23:45:26 -0300

    DEV: semaphore functions using adt

--- Kernel/include/libs/semaphore.h
+++ Kernel/include/libs/semaphore.h
@@ -2,16 +2,19 @@
 #define _SEMAPHORE_H
 
 #include <types.h>
+#include <ADTS/semCollectionADT.h>
+#include <libs/processManager.h>
+#include <libs/scheduler.h>
 
 extern int _xadd(int inc, int * value);
 extern int _xchg(int * lock, int value);
 
 sem_t sem_open(const char * name, uint32_t value);
 
-int64_t sem_wait(sem_t * sem);
+int64_t sem_wait(sem_t sem);
 
-int64_t sem_post(sem_t * sem);
+int64_t sem_post(sem_t sem);
 
-int64_t sem_close(sem_t * sem);
+int64_t sem_close(sem_t sem);
 
 #endif // _SEMAPHORE_H
--- Kernel/libs/semaphore.c
+++ Kernel/libs/semaphore.c
@@ -2,42 +2,128 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <libs/semaphore.h>
 
-typedef int lock_t;
 
-void acquire(lock_t *lock){
-  while(_xchg(lock, 1) != 0);
+// ====================== Definitions =================================
+
+// Semaphore data
+static semCollectionADT semCollection = NULL;
+
+// Auxiliary functions
+static void acquire(lock_t *lock);
+static void release(lock_t *lock);
+
+// ==================== Public Functions ====================
+// Creates semaphore manager
+void initSemaphores() { semCollection = newSemCollection(); }
+
+// Free semaphore manager
+void freeSemaphores() { freeSemCollection(semCollection); }
+
+// Creates a new semaphore with the given name and initial value
+// If the semaphore already exists, it returns the existing one
+sem_t sem_open(const char *name, uint32_t value) {
+    return getSem(semCollection, name, value);
 }
 
-void release(lock_t *lock){
-  _xchg(lock, 0);
+// Closes the semaphore
+int64_t sem_close(sem_t sem) {
+    // Check if the semaphore exists
+    if (!semExists(semCollection, sem)) {
+        return -1;
+    }
+
+    // TODO: what shoud happen to waiting processes?
+    return closeSem(semCollection, sem);
 }
 
-static lock_t globalMutex = 0;
+// Waits for the semaphore to be available
+// If the semaphore is available, it decrements it and returns 0
+// If the semaphore is not available, it blocks the process
+int64_t sem_wait(sem_t sem) {
 
-sem_t sem_open(const char * name, uint32_t value) {
+    // Check if the semaphore exists
+    if (!semExists(semCollection, sem)) {
+        return -1;
+    }
 
-    // TODO: check if resource name is available
-    //if(!nameAvailable) {
-    //  return -1;
-    //}
+    // Get the semaphore data
+    lock_t semLock = getSemLock(semCollection, sem);
+    uint64_t semValue = getSemValue(semCollection, sem);
 
-    sem_t out;
-    out.value = value;
-    return out;
+    // Lock the semaphore
+    acquire(&semLock);
+
+    // Check if the semaphore is available
+    if (semValue > 0) {
+
+        // Semaphore available
+        decSemValue(semCollection, sem);
+
+        // Unlock the semaphore
+        release(&semLock);
+
+        return 0;
+    }
+
+    // Semaphore not available
+    // Add the process to the waiting queue
+    addWaitingProcess(semCollection, sem, getActivePid());
+
+    // Unlock the semaphore
+    release(&semLock);
+
+    // Block the process
+    block(getActivePid());
+
+    return 0;
 }
 
-int64_t sem_wait(sem_t * sem) {
-    acquire(&globalMutex);
+// Increments the semaphore value
+// If there are processes waiting for the semaphore, it unblocks one of them
+// If there are no processes waiting for the semaphore, it increments the value
+int64_t sem_post(sem_t sem) {
 
-    release(&globalMutex);
+    // Check if the semaphore exists
+    if (!semExists(semCollection, sem)) {
+        return -1;
+    }
+
+    // Get the semaphore data
+    lock_t semLock = getSemLock(semCollection, sem);
+    uint64_t semValue = getSemValue(semCollection, sem);
+
+    // Lock the semaphore
+    acquire(&semLock);
+
+    // Check if there are processes waiting for the semaphore
+    if (getWaitingQueueSize(semCollection, sem) > 0) {
+
+        // Unblock a process
+        int pid = getNextWaitingProcess(semCollection, sem);
+        unblock(pid);
+
+        // Unlock the semaphore
+        release(&semLock);
+
+        return 0;
+    }
+
+    // No processes waiting for the semaphore
+    // Increment the value
+    incSemValue(semCollection, sem);
+
+    // Unlock the semaphore
+    release(&semLock);
+
+    return 0;
 }
 
-int64_t sem_post(sem_t * sem) {
-    acquire(&globalMutex);
-
-    release(&globalMutex);
+// ==================== Auxiliary Functions ====================
+// Acquire the lock
+static void acquire(lock_t *lock) {
+    while (_xchg(lock, 1) != 0);
 }
 
-int64_t sem_close(sem_t * sem);
-
+// Release the lock
+static void release(lock_t *lock) { _xchg(lock, 0); }
 
