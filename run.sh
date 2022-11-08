d7ab117a8531d64e0da0a97ee21b02bb642e2beb
Gonzalo Martone <gmartone@itba.edu.ar>
2022-11-07 23:38:55 -0300

    DEV: added MM variable

--- run.sh
+++ run.sh
@@ -4,22 +4,26 @@ if [ -z $SO_IMAGE ]; then
 fi
 
 name=TP2SO
+MM="BITMAP"
 # abro docker y ejecuto los make necesarios
 docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name $name $SO_IMAGE
 docker exec -it $name make clean        --directory=/root/Toolchain
 docker exec -it $name make all          --directory=/root/Toolchain
 docker exec -it $name make clean        --directory=/root
 if [ "$1" == "gdb" ]; then
-    docker exec -it $name make gdb      --directory=/root
+    docker exec -it $name make gdb MM=$MM     --directory=/root
 elif [ "$1" == "pvs" ]; then
-    docker exec -it $name make pvs      --directory=/root
+    docker exec -it $name make pvs MM=$MM     --directory=/root
 elif [ "$1" == "cpp-check" ]; then
     docker exec -it $name make cpp-check --directory=/root
 else
-    docker exec -it $name make all      --directory=/root
+    docker exec -it $name make all MM=$MM --directory=/root
 fi
 # al cambiar los permisos a 006 anda sin pedir sudo la ejecucion de qemu
-docker exec -it $name chmod 006 /root/Image/x64BareBonesImage.qcow2
+if [ "$1" != "cpp-check" ]; then
+    docker exec -it $name chmod 006 /root/Image/x64BareBonesImage.qcow2
+fi
+
 docker stop $name
 docker rm $name
 
